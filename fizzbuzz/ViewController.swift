5ac63eec8139837a9d10372b54344a01ccb7a9c4
atarubift <atarubift@gmail.com>
2022-09-17 15:13:45 +0900

    Initialize commit

--- fizzbuzz/ViewController.swift
+++ fizzbuzz/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var count = 0
 
+    @IBOutlet weak var countLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        countLabel.text = String(count)
     }
-
+    @IBAction func pulsButton(_ sender: Any) {
+        count += 1
+        checkFizzbuzz()
+    }
+    @IBAction func minusButton(_ sender: Any) {
+        if count >= 1 {
+            count -= 1
+        } else {
+            count = 0
+        }
+        checkFizzbuzz()
+    }
+    
+    func checkFizzbuzz() {
+        if count % 15 == 0 {
+            countLabel.text = "Fizz Buzz"
+        } else if count % 3 == 0 {
+            countLabel.text = "Fizz"
+        } else if count % 5 == 0 {
+            countLabel.text = "Buzz"
+        } else {
+            countLabel.text = String(count)
+        }
+    }
 
 }
 
